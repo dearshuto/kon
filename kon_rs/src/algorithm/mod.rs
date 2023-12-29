449b3e35ba6b4ede7b52025610c7cc5c18b563f8
Shuto Shikama <@gmail.com>
2023-12-29 16:31:58 +0900

    取得した HTML から情報を抽出する機能を追加

--- kon_rs/src/algorithm/mod.rs
+++ kon_rs/src/algorithm/mod.rs
@@ -1,5 +1,13 @@
 mod definition;
+mod html_parser;
 mod scheduler;
 
 pub use definition::{RoomMatrix, Schedule};
+pub use html_parser::HtmlParser;
 pub use scheduler::{Condition, Scheduler};
+
+pub struct BandSchedule {
+    pub name: String,
+    pub is_available: Vec<bool>,
+    pub members: Vec<String>,
+}
