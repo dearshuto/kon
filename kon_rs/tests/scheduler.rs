e6a6425ede8488b1a5a5b17c53ae3578ca8d194f
Shuto Shikama <@gmail.com>
2024-07-23 19:56:07 +0900

    テスト拡充

--- kon_rs/tests/scheduler.rs
+++ kon_rs/tests/scheduler.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use kon_rs::algorithm::{create_live_info, RoomMatrix, Scheduler};
 
@@ -71,4 +71,40 @@ fn simple_none() {
     let scheduler = Scheduler::new();
     let result = scheduler.assign(&room_matrix, &live_info);
     assert_eq!(result.len(), 0);
+}
+
+#[test]
+fn simple_() {
+    // 3 部屋 各 1 枠ずつ
+    // 枠数は足りているけど band_x と band_y は同時刻に入れないので解なし
+    let room_matrix = RoomMatrix::builder().push_room(5).build();
+    let room_matrix = Arc::new(room_matrix);
+    let band_table = HashMap::from([
+        ("band_x".to_string(), vec!["a".to_string()]),
+        ("band_y".to_string(), vec!["b".to_string()]),
+        ("band_z".to_string(), vec!["c".to_string()]),
+        ("band_u".to_string(), vec!["d".to_string()]),
+        ("band_v".to_string(), vec!["e".to_string()]),
+    ]);
+    let band_schedule: HashMap<String, Vec<bool>> = band_table
+        .keys()
+        .map(|key| (key.to_string(), vec![true; 5]))
+        .collect();
+    let live_info = create_live_info(&band_table, &band_schedule, &room_matrix);
+    let live_info = Arc::new(live_info);
+
+    let scheduler = Scheduler::new();
+    let result = scheduler.assign(&room_matrix, &live_info);
+    assert_eq!(result.len(), 120);
+
+    // 並列実行
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Failed building the Runtime")
+        .block_on(async {
+            let scheduler = Scheduler::new();
+            let result = scheduler.assign_async(room_matrix, live_info).await;
+            assert_eq!(result.len(), 120);
+        });
 }
