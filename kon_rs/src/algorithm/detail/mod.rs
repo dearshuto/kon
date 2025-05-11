5741ff5ab1e3bb58c21503a82872c2d491a6cd86
Shuto Shikama <@gmail.com>
2025-05-11 21:49:08 +0900

    順列新実装

--- kon_rs/src/algorithm/detail/mod.rs
+++ kon_rs/src/algorithm/detail/mod.rs
@@ -1,10 +1,12 @@
 mod partial_permutation;
 mod permutation_treverser;
+mod permuted_sequence;
 mod pruning_decorators;
 mod scheduler_impl;
 pub mod util;
 
 pub use partial_permutation::PartialPermutation;
+pub use permuted_sequence::PermutedSequence;
 pub use pruning_decorators::{
     BandScheduleTraverseDecorator, MemberConflictTraverseDecorator, TreeTraverser,
 };
